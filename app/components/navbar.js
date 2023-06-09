debcd8da5d3a768150b6ac542492c398daf6ae22
13jksingh <jaskeerat56.93@gmail.com>
2023-06-09 11:56:30 +0530

    changes made for deployment

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -25,14 +25,14 @@ const Navbar = ({ toggleDarkMode }) => {
                         {darkMode ? <CiLight /> : <MdDarkMode />}
                     </div>
                 </button>
-                {status === 'loading' ? <>     <div className=" border-t-teal-400 w-8 h-8 border-4 border-blue-200 rounded-full animate-spin"></div>
+                {/* {status === 'loading' ? <>     <div className=" border-t-teal-400 w-8 h-8 border-4 border-blue-200 rounded-full animate-spin"></div>
                     <span class="ml-2 text-base">loading</span></> : status === 'authenticated' ? (
                         <>
                             <img className=' rounded-full hover:border-2 border-emerald-300 transition duration-200' width={40} height={40} src={data.user.image} alt={data.user.name + ' photo'} />
                             <button className="border border-red-500 bg-red-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline text-base" onClick={signOut}>Sign Out</button>
                         </>
                     ) : <button className=" text-base border border-green-500 bg-green-500 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-600 focus:outline-none focus:shadow-outline" onClick={() => signIn('google')}>Sign In</button>
-                }
+                } */}
 
 
 
